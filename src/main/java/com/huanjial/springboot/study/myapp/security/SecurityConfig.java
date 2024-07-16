ddd06eb17a62874e2161a14a71183eec004c3d37
Huanjia Liang <huanjial@uci.edu>
2024-07-16 14:34:16 -0700

    Added Spring boot Security in pom to prompt user login, added UserDetailsManager and jdbcUserDetailManager to manage users from mysql database, added SecurityFilterChain to add authorities of calling APIs for each users

---
+++ src/main/java/com/huanjial/springboot/study/myapp/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.huanjial.springboot.study.myapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT user_id, pw, active from members where user_id = ?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT user_id, role from roles where user_id = ?");
+
+        return jdbcUserDetailsManager;
+    }
+
+
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN")
+
+                );
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable CSRF
+        // in general, not required for stateless APIs that use POST, PUT, DELETE
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
